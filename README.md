590b1ea5e6da8b281bc80abe730a824a480a4591
canwdev <c2097@live.com>
2022-01-21 14:42:26 +0800

    easy-peasy

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # React useContext + useReducer Demo
 
-- [useContext + useReducer](./src/App.js)
-- [Redux](./src/AppRedux.js)
+- [useContext + useReducer](./src/index.js)
+- [Redux](./src/index-redux.js)
+- [easy-peasy](./src/index-ep.js)
 
 ![](./screenshot.png)
--- src/index-ep.js
+++ src/index-ep.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './AppEp';
+import store from './easy-peasy/store'
+import {StoreProvider} from 'easy-peasy'
 
 ReactDOM.render(
   <React.StrictMode>
+    <StoreProvider store={store}>
       <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
